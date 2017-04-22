fbe3fdf9ae330c83cd99c67c2215728a8322f6a0
mike stedman <github@ravuya.com>
2017-04-21 21:31:13 -0600

    targeted imports from aeson make this cleaner

--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -2,12 +2,12 @@
 module Main where
 
 import Web.Scotty
-import Data.Aeson
+import Data.Aeson (object, (.=))
 
 main = do
   putStrLn "starting server"
   scotty 3000 $ do
     get "/caw/:caw_count" $ do
       count <- param "caw_count"
-      Web.Scotty.json (object [ "noises" .= (replicate count payload)]) where
+      json (object [ "noises" .= (replicate count payload)]) where
         payload = "caw" :: String
